1eccf0276f77b8c2eb642df50c3f8a27a98782ba
Patric Fornasier <patric.fornasier@gmail.com>
2020-03-23 14:54:21 +0100

    Summary class and tidy up.

--- index.js
+++ index.js
@@ -4,29 +4,45 @@ const stringify = require('json-stringify-pretty-compact');
 const categorise = require('./lib/categorise');
 const csv = require('./lib/csv');
 const table = require('./lib/table');
-const summarise = require('./lib/summary');
+const Summary = require('./lib/summary');
 
 const ENCODING = 'latin1';
 
+const options = yargs
+  .usage('Usage: -f <file>')
+  .option('f', {
+    alias: 'file',
+    type: 'string',
+    demandOption: true,
+  })
+  .option('j', {
+    alias: 'json',
+    type: 'boolean',
+    describe: 'print result in json instead of table format',
+  }).argv;
+
 module.exports = () => {
-  const options = yargs
-    .usage('Usage: -f <file>')
-    .option('f', {
-      alias: 'file',
-      type: 'string',
-      demandOption: true,
-    })
-    .option('j', {
-      alias: 'json',
-      type: 'boolean',
-      describe: 'print result in json instead of table format',
-    }).argv;
-
   const data = fs.readFileSync(options.file, { encoding: ENCODING });
-  const rows = csv.parse(data);
-  rows.forEach(categorise);
-  const summary = summarise(rows);
+  const rows = filter(csv.parse(data));
 
-  if (options.json) console.log(stringify(summary));
+  rows.forEach(categorise);
+  uncategorised(rows).forEach(r => console.log(asString(r)));
+
+  const summary = new Summary(rows);
+  if (options.json) console.log(stringify(summary.data));
   else console.log(table(summary));
+};
+
+const filter = rows => {
+  return rows
+    .filter(r => r.date.year() === 2020) // hack because can't restrict FKB exports by valuta
+    .filter(r => r.amount < 0 && r.category !== 'ignore');
+};
+
+const uncategorised = rows => {
+  return rows.filter(r => r.category === categorise.noCategory);
+};
+
+const asString = row => {
+  return `${row.date.toISOString()} ${row.description} ${row.amount}`;
 };
