7be096184556e679c8bc4f58b10faa85215eb04f
Patric Fornasier <patric.fornasier@gmail.com>
2020-03-23 13:45:26 +0100

    Pretty complete (table, months and category totals).

--- index.js
+++ index.js
@@ -1,63 +1,32 @@
 const fs = require('fs');
 const yargs = require('yargs');
+const stringify = require('json-stringify-pretty-compact');
 const categorise = require('./lib/categorise');
 const csv = require('./lib/csv');
+const table = require('./lib/table');
+const summarise = require('./lib/summary');
 
 const ENCODING = 'latin1';
 
 module.exports = () => {
-  const options = yargs.usage('Usage: -f <file>').option('f', {
-    alias: 'file',
-    type: 'string',
-    demandOption: true,
-  }).argv;
+  const options = yargs
+    .usage('Usage: -f <file>')
+    .option('f', {
+      alias: 'file',
+      type: 'string',
+      demandOption: true,
+    })
+    .option('j', {
+      alias: 'json',
+      type: 'boolean',
+      describe: 'print result in json instead of table format',
+    }).argv;
 
   const data = fs.readFileSync(options.file, { encoding: ENCODING });
   const rows = csv.parse(data);
   rows.forEach(categorise);
-
-  // console.log(rows.splice(0, 5));
-
   const summary = summarise(rows);
 
-  console.log(summary);
-};
-
-const summarise = rows => {
-  const result = {
-    all: { transactions: 0, total: 0 },
-    not_categorised: { transactions: 0, total: 0 },
-  };
-
-  rows
-    .filter(row => row.amount < 0 && row.category !== 'ignore')
-    .forEach(row => {
-      result.all.transactions += 1;
-      result.all.total += Math.round(row.amount);
-
-      if (row.category == undefined) {
-        result.not_categorised.transactions += 1;
-        result.not_categorised.total += Math.round(row.amount);
-        console.log(
-          'NO CAT: ' + row.date + ' ' + row.description + ' ' + row.amount
-        );
-      } else {
-        if (result[row.category] == undefined)
-          result[row.category] = { transactions: 0, total: 0 };
-
-        result[row.category].transactions += 1;
-        result[row.category].total += Math.round(row.amount);
-      }
-
-      // console.log(row.amount + ' ' + row.description);
-      // console.log(result.all.total);
-    });
-
-  // const result = {
-  //   all: { transactions: 42, total: 2233 },
-  //   groceries: { transactions: 42, total: 2233 },
-  //   not_categorised: { transactions: 42, total: 2233 },
-  // };
-
-  return result;
+  if (options.json) console.log(stringify(summary));
+  else console.log(table(summary));
 };
