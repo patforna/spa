c5a2ebf0633d0cc4343ba145180c666f12d48da6
Patric Fornasier <patric.fornasier@gmail.com>
2020-12-30 10:56:43 +0100

    Add end to end test.

--- acceptance.test.js
+++ acceptance.test.js
@@ -1,3 +1,25 @@
+const summaryx = require('./lib/summary');
+
+const csv = `Kontoauszug bis: 06.12.2020 ;;;
+;;;
+Kontonummer: 30 01 511.262-00;;;
+Bezeichnung: Lohnkonto;;;
+Saldo: CHF 6120.12;;;
+;;;
+Muster Hans & Muster Anna;;;
+Seefeldstrasse 12;;;
+8008 Zürich;;;
+;;;
+;;;
+Datum;Buchungstext;Betrag;Valuta
+04.12.20;E-Banking-Auftrag Muster Anna;-281.65;03.12.20
+04.12.20;Zahlung - Coop-5178 ZH Seefeldst, Zürich - 02.12.2020 18:40 - Karten-Nr. xxxxxxxxxxxx6107;-1.25;02.12.20
+03.12.20;Zahlung - Coop-2623 City SM, Zürich - 01.12.2020 15:29 - Karten-Nr. xxxxxxxxxxxx7837;-19.35;01.12.20
+03.12.20;Zahlung - Migros ALN Kreuzplatz, Zürich - 01.12.2020 10:00 - Karten-Nr. xxxxxxxxxxxx7837;-8.05;01.12.20`;
+
 test('should pass end to end', () => {
-  expect(sum(1, 2)).toBe(3);
+  const summary = summaryx.createFromCSV(csv);
+  const { amount, transactions } = summary.total();
+  expect(amount).toBe(310);
+  expect(transactions).toBe(4);
 });
